c1645d91116238d425bf2fba50d1cde9753e0c89
pbj1203 <pbj7922@gmail.com>
2014-06-23 19:09:17 +0800

    Update new discovery function, and add judgement for hardware support, enhance the classical bluetooth functions...

--- src/com/adatronics/bledeveloptool/DashBoard.java
+++ src/com/adatronics/bledeveloptool/DashBoard.java
@@ -1,13 +1,17 @@
 package com.adatronics.bledeveloptool;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * @author BojunPan@adatronics
@@ -16,18 +20,25 @@ import android.widget.Button;
  */
 public class DashBoard extends Activity implements OnClickListener{
 
+	
+	private static final int REQUEST_DISCOVERABLE = 0;	
+	private static final int DISCOVERABLE_DURATION = 300;
+	View btn_bleButton;
+	View btn_bluetoothButton;
+	Button btn_testButton;
+	View btn_aboutButton;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_dash_board);
-		View btn_bleButton = (Button) findViewById(R.id.btn_ble);
+		btn_bleButton = (Button) findViewById(R.id.btn_ble);
 		btn_bleButton.setOnClickListener(this);
-		View btn_bluetoothButton = (Button) findViewById(R.id.btn_bluetooth);
+		btn_bluetoothButton = (Button) findViewById(R.id.btn_bluetooth);
 		btn_bluetoothButton.setOnClickListener(this);
-		View btn_testButton = (Button) findViewById(R.id.btn_test);
+		btn_testButton = (Button) findViewById(R.id.btn_test);
 		btn_testButton.setOnClickListener(this);
-		View btn_aboutButton = (Button) findViewById(R.id.btn_about);
+		btn_aboutButton = (Button) findViewById(R.id.btn_about);
 		btn_aboutButton.setOnClickListener(this);
 	}
 
@@ -36,7 +47,12 @@ public class DashBoard extends Activity implements OnClickListener{
 		// TODO Auto-generated method stub
 		switch(v.getId()) {
 		case R.id.btn_ble:
-			Log.d("choose button","btn_ble");
+			if (!getPackageManager().hasSystemFeature(
+					PackageManager.FEATURE_BLUETOOTH_LE)) {
+				Toast.makeText(this, "The device doesn't support BLE",
+						Toast.LENGTH_SHORT).show();
+				return;
+			}
 			Intent btn_ble = new Intent(this, BleScan.class);
 			startActivity(btn_ble);
 			break;
@@ -45,14 +61,38 @@ public class DashBoard extends Activity implements OnClickListener{
 			startActivity(btn_bluetooth);
 			break;
 		case R.id.btn_test:
-			Intent btn_test = new Intent(this, TestTools.class);
-			startActivity(btn_test);
+			Intent i = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+			i.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, DISCOVERABLE_DURATION);
+			startActivityForResult(i, REQUEST_DISCOVERABLE);
 			break;
 		case R.id.btn_about:
 			Intent btn_about = new Intent(this, About.class);
 			startActivity(btn_about);
 			break;
 		}
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+
+		if (requestCode == REQUEST_DISCOVERABLE) {
+			if (resultCode == DISCOVERABLE_DURATION) {
+				btn_testButton.setEnabled(false);
+				btn_testButton.setText("... Advertising ...");
+				new Handler().postDelayed(new Runnable() {
+
+					@Override
+					public void run() {
+						// TODO Auto-generated method stub
+						btn_testButton.setEnabled(true);
+						btn_testButton.setText("Discoverable");
+					}
+				}, DISCOVERABLE_DURATION*1000);
+			}
+		} 
+		
+
 	}
 
 }
